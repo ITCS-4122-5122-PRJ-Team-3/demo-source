b8f91fc6f25775b8416f435a65de0e0a051e88e9
Alec Goncharow <Alec.Goncharow@gmail.com>
2021-03-22 21:57:57 -0400

    add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push --set-upstream https://github.com/ITCS-4122-5122-PRJ-Team-3/demo.git main -f
+
+cd -
