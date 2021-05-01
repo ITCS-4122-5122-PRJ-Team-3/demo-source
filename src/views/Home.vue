93874064cd3fb4b1b82bdad7a98461bc97604951
Rachel Conforti <50918318+rconforti97@users.noreply.github.com>
2021-05-01 19:47:32 -0400

    Updated Landing Page
    Added more background, added the how we built our project section, and added more to the 'who can use this information' section.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="home">
     <p>
-    Welcome to project team 3’s final project. We are Zachary Onisick, Iryna Naminat,  Alec Goncharow, Rachel Conforti.
+    Welcome to project team 3’s final project for ITCS 4122/5122 - Visual Analytics call of Spring 2021. We are Zachary Onisick, Iryna Naminat, Alec Goncharow, and Rachel Conforti.
     </p>
 
 
   <h3>Background:</h3>
   <p>
-  The dataset we have chosen has data on over ten thousand customers on various categories such as age, salary, marital status, education levels, dependents, credit card limit, and income category. 
-  We choose this dataset because there is a lot of information we pull from this dataset. For example, we can make inferences about how different groups of customers handle their money, potential contributing factors, 
-  and just overall interesting information about credit card users. Our main goal with our project was to give the user the ability to pick and choose what they would like to compare as well as answer a try to answer a few of 
-  our own questions.  
+  The dataset we have chosen is called 'Credit Card customers' from Kaggle, this dataset has information on over ten thousand customers on various categories such as age, salary, marital status, education levels, dependents, credit card limit, and income category. 
+  We picked this dataset because there is a large amount of information we can pull from this dataset that will allow us to show insightful information on the topic of credit card use.
+  For example, we can make inferences about how different groups of customers handle their money, potential contributing factors, what factors contribute to credit card limits or usage, etc.
+  Our main goal with our project is to give the user the ability to answer their questions and give them as much freedom to do so. The user will be presented the data displayed in multiple different ways,
+  to really help users come to their own conclusions and hopefully find answers to questions they never knew they had. We have laid out our project into two tabs under 'Visualizations', comparison and relationships.
+  The comparison tab will allow users to see multiple visualizations to compare and contrast different categories within the dataset. And for the relationship tab, we have placed our linear regression so users can see the relationships between
+  the data.
+
+  <h3>How we built our project:</h3>
+  We decided to use Vue 3 to build our project because it is very versatile. It allowed us to combine what we learned in class, such as ApexCharts, and bring new elements into our project. 
+  We also liked Vue because of its adaptability and how easy it is to work with outside libraries which allowed us to easily intergrate multiple libraries into our project. 
+  And lastly, we liked the look and feel that Vue could bring us with the animations and layout of our project since we wanted it to be very user-focused and allow them to use multiple visualizations 
+  at once.
   </p>
 
   <h3>Who can use this information:</h3>
   <p>
-  We believe this visualization could be used by many different people. It can be used by banks to figure out patterns within different groups of customers and show insightful information. 
-  Our visualizations can be used to educate students, customers, or even staff on what can happen in credit scenarios. 
-  And lastly, any user interested in credit trends and contributing factors can use these visualizations to answer questions.
+  We believe this visualization could be used by many different groups/types of people. It can be used by banks to figure out patterns within different groups of customers, show insightful information, and see potential contributing
+  factors to credit limit, credit balance, etc. Another use can be to educate students, customers, or employees on credit card usage scenarios. And lastly, any user interested in credit trends and contributing factors 
+  can use these visualizations to answer questions, find new questions, or just to see how different factors work together within the dataset.
   </p>
 
   </div>
